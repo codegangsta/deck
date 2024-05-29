a461778e28a65b6923254eea155e223f8c0cb9e3
Jeremy Saenz <jeremy.saenz@gmail.com>
2024-05-29 12:31:23 -0700

    More slides styling

--- src/components/slide.tsx
+++ src/components/slide.tsx
@@ -3,7 +3,6 @@ import { cn } from "../util/style";
 
 interface Props {
   id: string
-  align?: "top" | "center"
 }
 
 export default function Slide(props: ParentProps<Props>) {
@@ -11,8 +10,7 @@ export default function Slide(props: ParentProps<Props>) {
   return (
     <div id={props.id} class="bg-zinc-900 w-full h-svh flex items-center justify-center">
       <div class={cn(
-        "scale-100 hd:scale-150 fhd:scale-[200%] w-[960px] h-[540px] flex",
-        props.align == "center" ? "items-center" : "items-start"
+        "scale-100 hd:scale-150 fhd:scale-[200%] w-[960px] h-[540px] flex items-center",
       )}>
         <div class="prose prose-2xl prose-invert p-20 max-w-none">
           {props.children}
