d1a42a4c77f6422c06bd5f31403848fc07f35a7c
Jeremy Saenz <jeremy.saenz@gmail.com>
2024-05-29 08:58:58 -0700

    Fixed alignment

--- src/components/slide.tsx
+++ src/components/slide.tsx
@@ -10,7 +10,7 @@ export default function Slide(props: ParentProps<Props>) {
     <div class="bg-zinc-900 w-full h-svh flex items-center justify-center">
       <div class={cn(
         "scale-100 hd:scale-150 fhd:scale-[200%] w-[960px] h-[540px] flex",
-        props.align == "top" ? "items-start" : "items-center"
+        props.align == "center" ? "items-center" : "items-start"
       )}>
         <div class="prose prose-2xl prose-invert p-20 max-w-none">
           {props.children}
