4caaa5fa1ccbac87afb05786cf236d5a6e221d50
Jeremy Saenz <jeremy.saenz@gmail.com>
2024-05-29 07:18:51 -0700

    Added mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: false
-  })]
+  }), mdx()]
 });
