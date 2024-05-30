42efb9aca4678e095104b68d0638494734f06e0d
Jeremy Saenz <jeremy.saenz@gmail.com>
2024-05-30 07:50:32 -0700

    Added the rest of the content

--- src/components/slide.tsx
+++ src/components/slide.tsx
@@ -8,11 +8,11 @@ interface Props {
 export default function Slide(props: ParentProps<Props>) {
 
   return (
-    <div id={props.id} class="bg-zinc-900 w-full h-svh flex items-center justify-center border-b border-zinc-700">
+    <div id={props.id} class="bg-zinc-900 w-full h-svh flex items-center justify-center border-b border-zinc-700 overflow-hidden">
       <div class={cn(
         "scale-100 hd:scale-150 fhd:scale-[200%] w-[960px] h-[540px] flex items-center",
       )}>
-        <div class="prose prose-2xl prose-invert p-20 max-w-none w-full prose-ul:leading-7">
+        <div class="prose prose-2xl prose-invert p-20 max-w-none w-full prose-ul:leading-7 prose-code:leading-6">
           {props.children}
         </div>
       </div>
