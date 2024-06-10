3993cc4347fb25ba38516daabdb0586b7b53642b
Jeremy Saenz <jeremy.saenz@gmail.com>
2024-06-10 14:02:16 -0700

    Added microservices slide deck and began modifying for new workshop

--- src/components/slide.tsx
+++ src/components/slide.tsx
@@ -12,7 +12,7 @@ export default function Slide(props: ParentProps<Props>) {
       <div class={cn(
         "scale-100 hd:scale-150 fhd:scale-[200%] w-[960px] h-[540px] flex items-center",
       )}>
-        <div class="prose prose-2xl prose-invert p-20 max-w-none w-full prose-ul:leading-7 prose-code:leading-6">
+        <div class="prose prose-2xl prose-invert p-20 max-w-none w-full prose-ul:leading-7 prose-code:leading-6 relative">
           {props.children}
         </div>
       </div>
